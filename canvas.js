9d5aae57405cfbcac5f0c37a76dce924233e2610
Jonathan Adams <jd.adams16@gmail.com>
2023-09-27 01:40:44 -0500

    Add simple animation of the snake moving horizontally.

--- canvas.js
+++ canvas.js
@@ -6,12 +6,13 @@ canvas.height = config.boardHeight;
 
 const ctx = canvas.getContext('2d');
 
-const drawGrid = function(ctx) {
-    const numCellsInRow = config.boardWidth / config.cellSize;
+let currentColumn = config.snakeStartColumn;
+let currentRow = config.snakeStartRow;
+const numCellsInRow = config.boardWidth / config.cellSize;
 
+const drawGrid = function(ctx) {
     for(let rowNum = 0; rowNum < numCellsInRow; rowNum++) {
         for(let colNum = 0; colNum < numCellsInRow; colNum++) {
-            console.error(`Drawing row ${ rowNum }, column ${ colNum }.`);
             ctx.fillStyle = (rowNum + colNum) % 2 === 0 ? config.primaryTileColor : config.secondaryTileColor;
             ctx.fillRect(colNum * config.cellSize, rowNum * config.cellSize, config.cellSize, config.cellSize);
         }
@@ -19,6 +20,13 @@ const drawGrid = function(ctx) {
 }
 
 const drawSnakeSegment = function(ctx, segmentColumn, segmentRow) {
+    if(segmentColumn < 0) {
+        segmentColumn += numCellsInRow;
+    }
+    if(segmentRow < 0) {
+        segmentRow += numCellsInRow;
+    }
+
     ctx.fillStyle = config.snakeColor;
     ctx.fillRect(segmentColumn * config.cellSize, segmentRow * config.cellSize, config.cellSize, config.cellSize);
 
@@ -37,11 +45,24 @@ const drawSnakeSegment = function(ctx, segmentColumn, segmentRow) {
     ctx.stroke();
 }
 
-const drawSnake = function(ctx) {
+const drawSnake = function(ctx, column, row) {
     for(let snakeSegment = 0; snakeSegment < config.snakeStartLength; snakeSegment++) {
-        drawSnakeSegment(ctx, config.snakeStartColumn - snakeSegment, config.snakeStartRow);
+        drawSnakeSegment(ctx, column - snakeSegment, row);
     }
 }
 
-drawGrid(ctx);
-drawSnake(ctx);
+const clearCanvas = function(ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+const animationLoop = function(ctx) {
+    clearCanvas(ctx);
+    drawGrid(ctx);
+    drawSnake(ctx, currentColumn, currentRow);
+
+    currentColumn = (currentColumn + 1) % numCellsInRow;
+
+    setTimeout(function() { animationLoop(ctx); }, 100);
+}
+
+animationLoop(ctx);
